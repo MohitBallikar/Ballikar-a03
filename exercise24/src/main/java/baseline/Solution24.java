01ca52d1dae27035cbebda99c7b1b4bb39b61a1c
MohitBallikar <mohitballikar@gmail.com>
2021-09-29 15:20:35 -0400

    psuedo code and skeleton

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,25 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Mohit Ballikar
+ */
+package baseline;
+import java.util.Scanner;
+public class Solution24 {
+/*Implement scanners to grab user input and determine if they are anagrams
+Prompt for user input
+Process the user inputs and compare if they are anagrams or shuffles of each other
+Display the result of the processing to the user
+ */
+
+    //this function will store the strings inputted by the user and process the comparison
+    static boolean isAnagram(char[] str1, char[] str2){
+
+
+    }
+
+    //main will contain scanners and will do the printing for the users
+    public static void main(String[] args) {
+
+    }
+
+}
