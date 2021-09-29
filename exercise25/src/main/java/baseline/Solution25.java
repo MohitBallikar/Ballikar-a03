cb6e1687742f31be55573f27f8ec137cf0f76b93
MohitBallikar <mohitballikar@gmail.com>
2021-09-29 17:02:00 -0400

    psuedo code and skeleton

---
+++ exercise25/src/main/java/baseline/Solution25.java
@@ -0,0 +1,29 @@
+package baseline;
+
+public class Solution25 {
+
+    /*
+    prompt for user input
+    passwordValidator must be used for the comparison of the password to the requirements
+    maybe use a string comparison function or an array of character(s) function for the actual processing
+    ensure that a single output statement is used (could be a separate function)
+        output is varied between: very weak, weak, strong, very strong, and unknown
+    generate test cases for the efficacy of each of the above labels/descriptions
+
+     */
+
+    public static int passwordValidator(password){
+
+
+        return 0;//would return each type of label maybe using switch cases
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+        passwordValidator(password);
+
+    }
+}
