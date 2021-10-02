ed4319eb6d39ec6a60130b44e3187f1cfe4efc36
MohitBallikar <mohitballikar@gmail.com>
2021-10-01 23:24:43 -0400

    updated code (not final), backup reasons

--- exercise25/src/main/java/baseline/Solution25.java
+++ exercise25/src/main/java/baseline/Solution25.java
@@ -1,4 +1,12 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Mohit Ballikar
+ */
 package baseline;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Solution25 {
 
@@ -12,18 +20,77 @@ public class Solution25 {
 
      */
 
-    public static int passwordValidator(password){
+    public static int passwordValidator(String password) {
+
+        int plen = password.length();
+        int pstr = 0;
+
+        String regex = "^[a-zA-Z0-9]+$";
+        Pattern pattern = Pattern.compile(regex);
 
 
-        return 0;//would return each type of label maybe using switch cases
-    }
+        for (int i = 0; i < plen; i++) {
+
+            if ((pstr == 0) && (plen < 8) && (password.charAt(i) >= '!' && password.charAt(i) <= '/' || password.charAt(i) >= ':' && password.charAt(i) <= '@' || password.charAt(i) >= '[' && password.charAt(i) <= '\'' || password.charAt(i) >= '{' && password.charAt(i) <= '~')) {
+                pstr = 0;
+                return pstr;
+            }
+
+            if ((plen <= 7) && (password.charAt(i) >= '0') && (password.charAt(i) <= '9')) {
+                pstr = -2;
+                return pstr;
+            }
+
+            if ((plen <= 7) && (password.matches("[a-zA-Z]+"))) {
+                pstr = -1;
+                return pstr;
+            }
+
+            Matcher matcher = pattern.matcher(password);
+            pstr = 1;
+
+            if ((pstr == 1) && (plen >= 8)) {
+                pstr = 2;
+                return pstr;
+            }
+
+
+            if ((pstr == 1) && (((password.charAt(i) >= '!') && (password.charAt(i) <= '/')) || ((password.charAt(i) >= ':') && (password.charAt(i) <= '@')) || ((password.charAt(i) >= '[') && (password.charAt(i) <= '\'')) || ((password.charAt(i) >= '{') && (password.charAt(i) <= '~')))) {
+                pstr = 3;
+                return pstr;
+            }
 
 
 
 
-    public static void main(String[] args) {
+            return pstr;
+        }
 
-        passwordValidator(password);
 
+        public static void main(String[]args){
+            Scanner pwdscan = new Scanner(System.in);
+
+            System.out.print("Enter a password: ");
+
+            String password = pwdscan.nextLine();
+
+
+            int score = passwordValidator(password);
+
+            if (score == -2)
+                System.out.printf("The password \'%s\' is a very weak password. ", password);
+
+            if (score == -1)
+                System.out.printf("The password \'%s\' is a weak password. ", password);
+
+            if (score == 0)
+                System.out.printf("The password \'%s\' is of unknown strength. ", password);
+
+            if (score == 2)
+                System.out.printf("The password \'%s\' is a strong password. ", password);
+
+            if (score == 3)
+                System.out.printf("The password \'%s\' is a very strong password. ", password);
+        }
     }
 }
---
+++ exercise25/src/test/java/baseline/Solution25Test.java
@@ -0,0 +1,30 @@
+package baseline;
+
+import baseline.Solution25;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution25Test {
+
+    @Test
+    void testVeryWeakPassword(){
+    assertEquals(-2,);
+    }
+
+    @Test
+    void testWeakPassword(){
+
+    }
+
+    @Test
+    void testStrongPassword(){
+
+    }
+
+    @Test
+    void testVeryStrongPassword(){
+
+    }
+
+
+}
