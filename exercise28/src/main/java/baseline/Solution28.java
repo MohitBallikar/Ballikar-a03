e91534e17b575455fb0f93c9af62e060f61fce42
MohitBallikar <mohitballikar@gmail.com>
2021-10-03 16:36:48 -0400

    pseudo code with skeleton, may change depending on how implementation goes

---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,29 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Mohit Ballikar
+ */
+package baseline;
+
+public class Solution28 {
+
+    public static void main(String[] args) {
+        loopfunct();
+    }
+
+    public static int loopfunct(){
+
+        int sum =0;
+        return sum;
+
+    }
+
+}
+
+/*
+Write a program that prompts the user for five numbers and computes the total of the numbers.
+    Within a loop:
+        Prompt user input
+        Store user input
+        Add user input for calculation
+   The above section can be done within a function that will be called from the main
+ */
