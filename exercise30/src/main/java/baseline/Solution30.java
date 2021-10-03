21596fa41c094136cf8950c7bfb9e624cc648778
MohitBallikar <mohitballikar@gmail.com>
2021-10-03 19:46:18 -0400

    pseudo and skeleton

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,33 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Mohit Ballikar
+ */
+package baseline;
+
+public class Solution30 {
+
+    public static void main(String[] args) {
+
+        timestable();
+
+    }
+
+    public static void timestable(){
+
+    }
+
+
+
+}
+
+
+/*
+Create a program that generates a multiplication table for the numbers 1 through 12 (inclusive).
+Use a nested loop to complete this program.
+Align each column within the table without using the tab character.
+
+Use two loops:
+    One for printing
+    One for formatting
+Since it is all printing will most likely not need test cases
+ */
