17b09bdaccf9b27e9c52ff660f4f7e934061e739
MohitBallikar <mohitballikar@gmail.com>
2021-10-03 23:23:11 -0400

    pseudo code and skeleton

---
+++ exercise37/src/main/java/baseline/Solution37.java
@@ -0,0 +1,33 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Mohit Ballikar
+ */
+package baseline;
+
+public class Solution37 {
+    public static void main(String[] args) {
+
+        generatepassword();
+
+    }
+
+    public static void generatepassword(){
+
+    }
+}
+
+/*
+Create a program that generates a secure password.
+Prompt the user for the minimum length, the number of special characters, and the number of numbers.
+Then generate a password for the user using those inputs.
+
+Use lists to store the characters you’ll use to generate the passwords.
+Ensure that the generated password is random.
+Ensure that there are at least as many letters as there are special characters and numbers.
+
+Function will have to be called within main
+Function will need to call math.random to ensure that inputs are unique
+Lists will have to be defined as well
+    Use loops for printing
+User input will have to be prompted as well as outputting the password to the user
+ */
