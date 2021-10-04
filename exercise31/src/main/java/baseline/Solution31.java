0d9ce794d0da949dde27cf77efe08ea52b8ae1cd
MohitBallikar <mohitballikar@gmail.com>
2021-10-03 20:07:58 -0400

    pseudo and comments

---
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -0,0 +1,26 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Mohit Ballikar
+ */
+package baseline;
+
+public class Solution31 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int heartratecalc(){
+        return 0;
+    }
+
+}
+
+/*
+Create a program that prompts for your age and your resting heart rate.
+Use the Karvonen formula to determine the target heart rate based on a range of intensities from 55% to 95%.
+Don’t hard-code the percentages. Use a loop to increment the percentages from 55 to 95.
+Ensure that the heart rate and age are entered as numbers. Don’t allow the user to continue without entering valid inputs.
+Display the results in a tabular format.
+    Formula = TargetHeartRate = (((220 − age) − restingHR) × intensity) + restingHR
+Needs to be formatted with the proper hyphens/underscores/pipes
+ */
