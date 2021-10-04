a5293d7d6555689c9c4aa4c571c6afc274ff6092
MohitBallikar <mohitballikar@gmail.com>
2021-10-03 22:30:27 -0400

    skeleton and pseudo

---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,34 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Mohit Ballikar
+ */
+package baseline;
+
+public class Solution34
+{
+    public static void main(String[] args) {
+
+        removeemployee();
+        displayemployees();
+    }
+
+    public static void removeemployee(){
+
+    }
+
+    public static void displayemployees(){
+
+    }
+}
+
+/*
+Create a small program that contains a list of employee names.
+Print out the list of names when the program runs the first time.
+Prompt for an employee name and remove that specific name from the list of names.
+Display the remaining employees, each on its own line.
+Use an array or list to store the names.
+    Will need to have a list of employee names
+    Print out the list (loop)
+    Prompt for an employee name to remove
+    Display rest of the untouched list
+ */
