e53dc366a71dfe6dba8eff4ceb36265e46402d7f
MohitBallikar <mohitballikar@gmail.com>
2021-10-03 23:58:58 -0400

    code

--- exercise26/src/main/java/baseline/PaymentCalculator.java
+++ exercise26/src/main/java/baseline/PaymentCalculator.java
@@ -1,12 +1,41 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Mohit Ballikar
+ */
 package baseline;
 
 public class PaymentCalculator {
-    int numofmonths = 0;
+    {
+        double balance;
+        int apr;
+        double monthlypayment;
 
+        public double calculateMonthsUntilPaidOff()
+        {
+            double noofmonths = 0;
+            //Calculate the Paid off here..
+            //n is the number of months.
+            //i is the daily rate (APR divided by 365).
+            //b is the balance.
+            //p is the monthly payment.
 
-}
+            double i = (double) apr / 365;
+            double b = balance;
+            double p = monthlypayment;
 
-   /* Use a class called PaymentCalculator with a public method called calculateMonthsUntilPaidOff, which takes no parameters and returns the number of months.
-        Round fractions of a cent up to the next cent for internal calculations
+            double tempvalue = (double) 1 + i;
+            double logvalue = Math.log(tempvalue);
+            double powtempvalue = Math.pow(tempvalue,30);
+            double tempmathlogvalue = Math.round(1 + (double) b/p * (1 - powtempvalue));
+            double mathlogvalue = (double) Math.log(-tempmathlogvalue);
+            double divide30 = (double)1/30;
+            double t = (double) divide30 * mathlogvalue;
 
-    */
+            //noofmonths = -(1/30) * log(1 + b/p * (1 - (1 + i)^30)) / log(1 + i);
+            noofmonths =  t/ (double) logvalue;
+            //System.out.println("Value of noofmonths " + Math.round(noofmonths) );
+
+            return noofmonths;
+
+        }
+    }
--- exercise26/src/main/java/baseline/Solution26.java
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -1,12 +1,59 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Mohit Ballikar
+ */
 package baseline;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 public class Solution26 {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException
+    {
+        //Calculate number of Months for paying off Credit Card.
+        calculateMonths();
     }
 
+    static void calculateMonths() throws IOException
+    {
+        int n = 0;
+        String inputString;
+        double returnvalue = 0;
+
+        //Define the standard library for inputs
+        BufferedReader readInput = new BufferedReader(new InputStreamReader(System.in));
+
+        //Initialize the public class.
+        PaymentCalculator pc = new PaymentCalculator();
+
+        //Get Inputs
+        System.out.print("What is your balance? ");
+        inputString = readInput.readLine();
+        pc.balance = Float.parseFloat(inputString);
+
+        System.out.print("What is the APR on the card (as a percent)? ");
+        inputString = readInput.readLine();
+        try
+        {
+            pc.apr = Integer.parseInt(inputString);
+        }
+        catch (NumberFormatException e)
+        {
+            System.out.println("Invalid APR.");
+        }
+
+        System.out.print("What is the monthly payment you can make? ");
+        inputString = readInput.readLine();
+        pc.monthlypayment = Float.parseFloat(inputString);
+
+        //Call the public method to calculate Months.
+        returnvalue = pc.calculateMonthsUntilPaidOff();
+
+        if (returnvalue > 0)
+        {
+            System.out.println("It will take you "+ 365/Math.round(returnvalue)+ " months to pay off this card");
+        }
+
+    }
 
 }
 
