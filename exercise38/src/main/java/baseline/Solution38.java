f58d15dcd2c3ed0cc6db1627d622319607fcfff6
MohitBallikar <mohitballikar@gmail.com>
2021-10-03 23:38:52 -0400

    pseudo and skeleton

---
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,32 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Mohit Ballikar
+ */
+package baseline;
+
+public class Solution38 {
+    public static void main(String[] args) {
+
+        processEvenNumbers();
+    }
+
+    static void processEvenNumbers(){
+
+    }
+}
+
+
+/*
+Create a program that prompts for a list of numbers, separated by spaces.
+Have the program print out a new list containing only the even numbers.
+
+Convert the input to an array.
+Many languages can easily convert strings to arrays with a built-in function that splits apart a string based on a specified delimiter.
+Write your own algorithm—don’t rely on the language’s built-in filter or similar enumeration feature.
+Use a function called filterEvenNumbers to encapsulate the logic for this.
+The function takes in the original array and returns the new array.
+
+Call function and ensure that the input is an array
+ensure spaces are accounted for, either in input or output processing
+declare even numbers (modulo)
+ */
