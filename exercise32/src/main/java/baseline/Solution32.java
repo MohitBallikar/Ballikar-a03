5877203d3804a849b3a4a7cf1bfb4ffbe760428d
MohitBallikar <mohitballikar@gmail.com>
2021-10-03 21:57:09 -0400

    code with test case may alter later

--- exercise32/src/main/java/baseline/Solution32.java
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -8,44 +8,123 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Random;
 public class Solution32 {
+    //main calls the game function
+    public static void main(String[] args) throws IOException
+    {
 
-    public static void main(String[] args) {
-
-            guessthenumber();
-    }
-
-    static void guessthenumber(){
+        guessthenumber();
 
     }
+    //does the randomization and the background of the game
+    static void guessthenumber() throws IOException
+    {
+        //initialize the values for each variable
+        int difficultylevel = 0;
+        int yourguess = 0;
+        int min = 0;
+        int max = 0;
+        int randomvalue = 0;
+        String inputstring = "";
+        System.out.println("Let's play Guess the Number!");
+
+        //int i=2 Add one for first call.
+
+        //Define the standard library for inputs
+        BufferedReader readInput = new BufferedReader(new InputStreamReader(System.in));
+
+        //loop ensures that the correct value is entered
+        while (true)
+        {
+            //new random number is made before each round/game
+            Random random = new Random();
+
+            //user selects difficulty
+            System.out.print("Enter the difficulty level (1, 2, or 3): ");
+            inputstring = readInput.readLine();
+
+            try
+            {
+                difficultylevel = Integer.parseInt(inputstring);
+
+                //hard codes the bounds for the min and max for each difficulty
+                //the random value will be in between these bounds
+
+                if (difficultylevel == 1 )
+                {
+                    min = 1;
+                    max = 10;
+                    randomvalue = random.nextInt(max + min) + min;
+                }
+
+                //difficultylevel = 2
+                if (difficultylevel == 2 )
+                {
+                    min = 1;
+                    max = 100;
+                    randomvalue = random.nextInt(max + min) + min;
+                }
+
+                // difficultylevel = 3
+                if (difficultylevel == 3 )
+                {
+                    min = 1;
+                    max = 1000;
+                    randomvalue = random.nextInt(max + min) + min;
+                }
+
+                //begins reading user guesses
+                System.out.print("I have my number. What's your guess?: ");
+                inputstring = readInput.readLine();
+                yourguess = Integer.parseInt(inputstring);
+                int i=1; // Add one for first call.
+
+
+                while (true)
+                {
+
+                    //System.out.println("i " + i);
+                    //ends loop if guessed correctly
+                    if ( randomvalue == yourguess )
+                    {
+                        break;
+                    }
+                    //custom prompts to lead the user to the correct answers
+                    else if ( yourguess < randomvalue)
+                    {
+                        System.out.println("Too low. Guess again: ");
+                        inputstring = readInput.readLine();
+                        yourguess = Integer.parseInt(inputstring);
+                    }
+                    else if ( yourguess > randomvalue)
+                    {
+                        System.out.println("Too High. Guess again: ");
+                        inputstring = readInput.readLine();
+                        yourguess = Integer.parseInt(inputstring);
+                    }
+
+                    //increment the counter after each guess
+                    i++;
+                }
+
+                //print out result and ask to play again
+                System.out.printf("You got it in %d guesses!\n",i);
+
+                System.out.println("Do you wish to play again (Y/N)? ");
+                inputstring = readInput.readLine();
+
+                if ( inputstring.equals("n") || inputstring.equals("N"))
+                {
+                    System.exit(0);
+                }
+
+
+            }
+            catch (Exception e) {
+                System.out.println("Inputs are not valid.");
+            }
+        }
+    }
+    }
 
 
 
-
-
-
-}
-
-
-/*
-The first level of difficulty would be a number between 1 and 10.
-The second difficulty set would be between 1 and 100.
-The third difficulty set would be between 1 and 1000.
-
-Prompt for the difficulty level, and then start the game.
-The computer picks a random number in that range and prompts the player to guess that number.
-Each time the player guesses,
-the computer should give the player a hint whether the number is too high or too low.
-The computer should also keep track of the number of guesses.
-Once the player guesses the correct number,
-the computer should present the number of guesses and ask if the player would like to play again.
-
-    Prompt for input, and then call a function which will house the game
-    Player guesses for a number that the computer/program defines
-    Player guess must be compared to the computer value
-    Each guess must be counted
-    Once guess == computer number, computer prints the number of guesses and asks if the player wants to play again
-
-Don’t allow any non-numeric data entry.
-During the game, count non-numeric entries as wrong guesses.
-
- */
---
+++ exercise32/src/test/java/baseline/Solution32Test.java
@@ -0,0 +1,26 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+class Solution32Test {
+
+    @Test
+    void testguesscounter(){
+    Solution32 app = new Solution32();
+
+    int expected = 10;
+    //int actual = app.guessthenumber();
+
+    }
+
+    @Test
+    void testranddiff1(){
+        Solution32 app = new Solution32();
+
+        int expected = 10;
+        //int actual = app.guessthenumber();
+    }
+
+}
