3c343275e5fc1132a8ac763d75b67f37712dd794
MohitBallikar <mohitballikar@gmail.com>
2021-10-03 23:07:35 -0400

    pseudo and skeleton

---
+++ exercise36/src/main/java/baseline/Solution36.java
@@ -0,0 +1,36 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Mohit Ballikar
+ */
+package baseline;
+
+public class Solution36 {
+    public static void main(String[] args) {
+
+    computestats();
+
+    }
+
+    static void computestats(){
+        
+    }
+
+
+}
+
+
+/*
+Write a program that prompts for response times from a website in milliseconds.
+It should keep asking for values until the user enters “done.”
+The program should print the average time (mean), the minimum time, the maximum time, and the population standard deviation.
+Create functions called average, max, min, and std, which take in a list of numbers and return the results.
+Use loops and arrays to perform the input and mathematical operations.
+Be sure to exclude the "done" entry from the inputs.
+Be sure to properly ignore any invalid inputs.
+Keep the input separate from the processing and the output.
+
+    Create function for each part of calculation
+    Loops for inputs from the user as well as reducing math overhead
+    ignore invalid inputs( exceptions)
+
+ */
