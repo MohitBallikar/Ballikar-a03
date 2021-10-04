c5d0b04769604c043f4b9ff4680f12cce8208ddb
MohitBallikar <mohitballikar@gmail.com>
2021-10-03 20:01:17 -0400

    final code

--- exercise30/src/main/java/baseline/Solution30.java
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -6,28 +6,24 @@ package baseline;
 
 public class Solution30 {
 
+    //calls the function
     public static void main(String[] args) {
 
         timestable();
 
     }
 
+    //function prints times table for twelve
     public static void timestable(){
-
+        //loops 12 times
+        for (int i = 1; i <= 12 ; i++)
+        {
+            //multiplies i in the print statement as well as printing the values through this statement
+            System.out.printf( "%-3s %3s %3s %3s %3s %3s %3s %3s %3s %3s %3s %3s%n", i, i*2,i*3,i*4, i*5, i*6, i*7, i*8, i*9, i*10, i*11, i*12);
+            //formatting is done with the format specifiers
+        }
     }
-
-
-
+    
 }
 
 
-/*
-Create a program that generates a multiplication table for the numbers 1 through 12 (inclusive).
-Use a nested loop to complete this program.
-Align each column within the table without using the tab character.
-
-Use two loops:
-    One for printing
-    One for formatting
-Since it is all printing will most likely not need test cases
- */
