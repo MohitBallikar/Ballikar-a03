684ec7ffc402c81fbd25e374f3ea209ae5a232cf
MohitBallikar <mohitballikar@gmail.com>
2021-10-03 23:54:50 -0400

    pseudo

---
+++ exercise40/src/main/java/baseline/Solution40.java
@@ -0,0 +1,21 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Mohit Ballikar
+ */
+package baseline;
+
+public class Solution40 {
+    public static void main(String[] args) {
+    searchRecords();
+    }
+    static void searchRecords(){
+        
+    }
+}
+
+/*
+Given the following data set create a program that lets a user locate all records that match the search string by comparing the search string to the first or last name field.
+ Similar to 39
+ ---->Store the data using a list of maps.
+
+ */
