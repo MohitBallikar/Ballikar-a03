2524d26fa26662f1ef6b0595a2552f57cc1fbb94
MohitBallikar <mohitballikar@gmail.com>
2021-10-03 23:47:51 -0400

    pseudo

---
+++ exercise39/src/main/java/baseline/Solution39.java
@@ -0,0 +1,26 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Mohit Ballikar
+ */
+package baseline;
+
+public class Solution39 {
+    public static void main(String[] args) {
+    sortrecords();
+    }
+
+
+    public static void sortrecords(){
+
+    }
+}
+
+/*
+Given the following data set,
+create a program that sorts all employees by last name and prints them to the screen in a tabular format.
+
+Store the data using a list of maps.
+
+Prompt user input and store in a map
+sort and print formatted
+ */
