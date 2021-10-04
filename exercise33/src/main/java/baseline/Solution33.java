5b294189796e7bb74d922ea6ffefc61a290d2811
MohitBallikar <mohitballikar@gmail.com>
2021-10-03 22:15:22 -0400

    pseudo and skeleton

---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,32 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Mohit Ballikar
+ */
+package baseline;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+public class Solution33 {
+    public static void main(String[] args) throws IOException{
+
+        playmagicball();
+    }
+
+    static void playmagicball() throws IOException{
+        
+    }
+}
+
+
+
+/*
+Create a Magic 8 Ball game that prompts for a question and then displays either "Yes," "No," "Maybe," or "Ask again later."
+The value should be chosen randomly using a pseudo random number generator.
+Store the possible choices in a list and select one at random.
+
+Prompt user input for a question to ask the program
+Call for a function that randomly pulls a response from an array
+
+ */
