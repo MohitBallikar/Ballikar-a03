70b605b3bca17a2c3401079db2cdb6ba3ef37002
MohitBallikar <mohitballikar@gmail.com>
2021-10-03 22:47:51 -0400

    pseudo and skeleton

---
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,31 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Mohit Ballikar
+ */
+package baseline;
+
+public class Solution35 {
+    public static void main(String[] args) {
+    pickawinner();
+    }
+
+
+    public static void pickawinner(){
+
+    }
+}
+
+
+/*
+Create a program that picks a winner for a contest or prize drawing.
+Prompt for names of contestants until the user leaves the entry blank.
+Then randomly select a winner.
+    Use a loop to capture user input into an array.
+    Use a random number generator to pluck a value from the array.
+    Don’t include a blank entry in the array.
+    Some languages require that you define the length of the array ahead of time. You may need to find another data structure, like an ArrayList.
+
+ Pick random individual from an array list
+ make sure all entries/indices are populated
+ Have the function be called to select the winner with the above parameters
+ */
